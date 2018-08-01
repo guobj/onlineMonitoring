c193f38638e3898661d09953dc916c763653e2dc
ssh <17854119841@163.com>
2018-08-01 10:29:22 +0800

    1

--- src/main/java/com/nz/onlineMonitoring/login/controller/LoginController.java
+++ src/main/java/com/nz/onlineMonitoring/login/controller/LoginController.java
@@ -1,14 +1,16 @@
 package com.nz.onlineMonitoring.login.controller;
 
-import com.nz.onlineMonitoring.login.model.Login;
-import com.nz.onlineMonitoring.login.service.LoginService;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.Map;
+import com.nz.onlineMonitoring.login.model.Login;
+import com.nz.onlineMonitoring.login.service.LoginService;
 
 @Controller
 public class LoginController {
@@ -28,7 +30,7 @@ public class LoginController {
 		try {
 			Login loginUser = loginService.login(login);
 			session.setAttribute("user", loginUser);
-			url = "station/listStation";
+			url = "redirect:/station/listStation";
 		} catch (Exception e) {
 			map.put("message", e.getMessage());
 		}
